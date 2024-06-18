e21e9d47c2dfa24c55bd18961763801d37ee28d8
oOwuowenOo <164132354+oOwuowenOo@users.noreply.github.com>
2024-06-18 20:25:23 +0800

    Add files via upload

---
+++ Blackjack/Blackjack/Source.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include"Game.h"
+using namespace std;
+
+int main() {
+    string player_name;
+    cout << "輸入名字: ";
+    cin >> player_name;
+
+    int n = 1;
+    while(n != -1)
+    {
+        Game game(player_name);
+        game.play();
+        cout << "輸入-1結束遊戲，輸入-1以外的【數字】繼續: ";
+        cin >> n;
+        if (n != -1) {
+            cout << "\n\n\n<<===================新的一局===================>>\n";
+        }
+    }
+
+    return 0;
+}
